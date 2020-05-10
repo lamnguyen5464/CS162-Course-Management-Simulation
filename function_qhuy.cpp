8e1c3a567a9fd6629df329430eb2aee57eb44fd7
BuiQuangHuy19125047 <quanghuybui93@gmail.com>
2020-05-10 12:34:51 +0700

    qhuy

--- function_qhuy.cpp
+++ function_qhuy.cpp
@@ -11,14 +11,13 @@ void staffClassMenu (CoreData &data){
     bool showOption = true;
     while (1){
         if (showOption){
-            cout << "__________________MENU__________________" << endl;
+            cout << "__________________STAFF-CLASS__________________" << endl;
             cout << "1.Import students of a class from the csv file." << endl;
             cout << "2.Manually add a student to a class." << endl;
             cout << "3.Edit an existing student" << endl;
             cout << "4.Remove a student" << endl;
             cout << "5.Change a student from class A to class B." << endl;
-            cout << "6.View list of classes." << endl;
-            cout << "7.View list of students in a class."<<endl;
+            cout << "6.View list of classes and list of students in a class." << endl;
             cout << "0.Return to previous menu." << endl;
         }
         else{
@@ -30,23 +29,24 @@ void staffClassMenu (CoreData &data){
         cin >> yourChoice;
         switch (yourChoice){
             case 0:
-
+                return;
             case 1:
                 importStudentFromCsvFile(data);
                 break;
             case 2:
-
+                addAStudent(data);
+                break;
             case 3:
-                editAnExistingStudent (data);
+                editAnExistingStudent(data);
+                break;
             case 4:
-
+                removeAStudent(data);
+                break;
             case 5:
-
+                 moveStudentFromAToB (data);
+                 break;
             case 6:
                 viewListOfClasses(data);
-                break;
-            case 7:
-                viewListOfStudents(data);
                 break;
             default:
                 showOption = false;
@@ -59,8 +59,10 @@ void importStudentFromCsvFile (CoreData &data){
     ifstream fin;
     string linkOfFile,nameOfClass;
     cout << "Which class do you want to import: ";
+    cin.ignore();
     getline(cin, nameOfClass);
     cout << "Please let us know the link of your csv file: ";
+    cin.ignore();
     getline(cin, linkOfFile);
     fin.open(linkOfFile);
     if (!fin.is_open()){
@@ -78,6 +80,7 @@ void importStudentFromCsvFile (CoreData &data){
             switch (choice){
                 case 2:
                     cout << "Which class do you want to import: ";
+                    cin.ignore();
                     getline(cin, nameOfClass);
                     break;
                 default:
@@ -115,7 +118,7 @@ void importStudentFromCsvFile (CoreData &data){
         fin.close();
     }
 }
-void edit(Student *&tmpSt,int choice){
+void edit(Student *&tmpSt,int choice,bool &checkChoice){
     switch(choice){
         case 1:{
             cout << "New ID: ";
@@ -128,6 +131,7 @@ void edit(Student *&tmpSt,int choice){
         case 2:{
             cout << "New last name: ";
             string tmpLast;
+            cin.ignore();
             getline(cin,tmpLast);
             tmpSt ->lastName = tmpLast;
             cout << "Edit successfully";
@@ -136,6 +140,7 @@ void edit(Student *&tmpSt,int choice){
         case 3:{
             cout << "New first name: ";
             string tmpFirst;
+            cin.ignore();
             getline(cin,tmpFirst);
             tmpSt ->firstName = tmpFirst;
             cout << "Edit successfully";
@@ -144,6 +149,7 @@ void edit(Student *&tmpSt,int choice){
         case 4:{
             cout << "New doB: ";
             string tmpdob;
+            cin.ignore();
             getline(cin,tmpdob);
             tmpSt ->dOB = tmpdob;
             cout << "Edit successfully";
@@ -152,91 +158,191 @@ void edit(Student *&tmpSt,int choice){
         case 5:{
             cout << "New gender: ";
             string tmpgender;
+            cin.ignore();
             getline(cin,tmpgender);
             tmpSt ->gender= tmpgender;
             cout << "Edit successfully";
             break;
         }
         default:
+            checkChoice = false;
             break;
     }
 }
-void editAnExistingStudent (CoreData &data){
-    long long ID;
-    cout << "Please input id of the student you want to edit: ";
-    cin >> ID;
-    Student *tmpSt=NULL;
-    Class *tmpClass=NULL;
-    int choice;
-    if (findStudent(ID,data,tmpSt,tmpClass)){
-        cout << "What part do you want to edit? "<<endl;
-        cout << "1.ID: " << tmpSt->id << endl;
-        cout << "2.Last name: " << tmpSt->lastName<< endl;
-        cout << "3.First name: " << tmpSt->firstName << endl;
-        cout << "4.DoB: " << tmpSt->dOB << endl;
-        cout << "5.Gender: " << tmpSt->id << endl;
-        cout << "Your choice: ";
-        cin >> choice;
-        edit(tmpSt,choice);
+void editAnExistingStudent(CoreData &data){
+    while(1){
+        long long ID;
+        bool checkChoice = true;
+        if (checkChoice == false) cout << "Your choice is invalid";
+        cout << "Please input id of the student you want to edit: ";
+        cin >> ID;
+        Student *tmpSt=NULL;
+        Class *tmpClass=NULL;
+        int choice;
+        if (findStudent(ID,data,tmpSt,tmpClass)){
+            cout << "What part do you want to edit? "<<endl;
+            cout << "1.ID: " << tmpSt->id << endl;
+            cout << "2.Last name: " << tmpSt->lastName<< endl;
+            cout << "3.First name: " << tmpSt->firstName << endl;
+            cout << "4.DoB: " << tmpSt->dOB << endl;
+            cout << "5.Gender: " << tmpSt->id << endl;
+            cout << "Your choice (0 to return): ";
+            cin >> choice;
+            if (choice == 0) return;
+            edit(tmpSt,choice,checkChoice);
 
-    }
-    else{
-        cout << "Can not find student"<<endl;
-        cout << "1.Input another student."<<endl;
-        cout << "2.Return";
-        cout << "Your choice: ";
-        cin >> choice;
-        switch(choice){
-            case 1:
-            //input another student.
-            default:
-                break;
+        }
+        else{
+            cout << "Can not find student"<<endl;
+            cout << "1.Input another student."<<endl;
+            cout << "2.Return" << endl;
+            cout << "Your choice: ";
+            cin >> choice;
+            switch(choice){
+                case 2:
+                    return;
+                default:
+                    break;
+            }
         }
     }
 }
 
 void viewListOfClasses(CoreData data){
-    Class *curClass = data.pHeadClass;
-    if (data.numOfClasses == 0) cout << "There is no class"<<endl;
-    else{
-        while (curClass != NULL){
-            cout << "Class: " << curClass ->name << " has " << curClass ->numOfStudents << "students." <<endl;
-            curClass = curClass ->next;
+    bool checkChoice = true;
+    while(1){
+        if (checkChoice == false) cout << "Your choice is invalid. "<<endl;
+        Class *curClass = data.pHeadClass;
+        if (data.numOfClasses == 0) {
+            cout << "There is no class"<<endl;
+            return;
+        }
+        else{
+            int a = 0,choice;
+            while (curClass != NULL){
+                cout<< a <<". " << "Class: " << curClass ->name << " has " << curClass ->numOfStudents << " students." <<endl;
+                curClass = curClass ->next;
+            }
+            cout << "Which class do you want to view list of students (0 to return): ";
+            cin >> choice;
+            if (choice == 0) return;
+            else if (choice <= a){
+                curClass = data.pHeadClass;
+                int i = 0;
+                while (i < choice){
+                    curClass = curClass ->next;
+                    i++;
+                }
+                viewListOfStudents(curClass);
+            }
+            else checkChoice = false;
         }
     }
-    //return to the previous menu.
 }
 
-void viewListOfStudents(CoreData data){
-    Class *tmpClass = NULL;
-    string classname;
-    int choice = 1;
-    cout << "Which class do you want to view list: ";
-    getline(cin,classname);
-    while(findClass(classname,data,tmpClass) == false && choice == 1){
-        cout << "Can not find this class"<<endl;
-        cout << "1.Please input again: " << endl;
-        cout << "2.Return: " <<endl;
-        cout << "Your choice: ";
-        cin >> choice;
-        switch(choice){
-            case 1:{
-                getline(cin,classname);
-                break;
-            }
+void viewListOfStudents(Class *curClass){
+    Student *tmpSt = curClass ->pHeadStudent;
+    if (tmpSt == NULL){
+        cout << "there is no student in this class"<<endl;
+    }
+    else{
+        cout << "---------------------------------------------------------------"<<endl;
+        while(tmpSt != NULL){
+            cout << "--------------------------------------"<<endl;
+            cout <<"| " << tmpSt->id <<" | " << tmpSt->lastName <<" | " << tmpSt->firstName <<" | "<< tmpSt->dOB <<" | " << tmpSt->gender<<" |"<<endl;
+        }
+        cout << "---------------------------------------------------------------"<<endl;
+    }
+}
+
+void addAStudent(CoreData &data){
+    while(1){
+        Student *tmpSt = new Student;
+        string classname;
+        cout << "Please input the student's ID: " ;
+        cin >> tmpSt->id;
+        cout << "Please input the student's last name: " ;
+        cin.ignore();
+        getline(cin,tmpSt->lastName);
+        cout << "Please input the student's first name: " ;
+        getline(cin,tmpSt->firstName);
+        cout << "Please input the student's DoB : " ;
+        getline(cin,tmpSt->dOB);
+        cout << "Please input the student's gender: " ;
+        getline(cin,tmpSt->gender);
+        cout << "Which class you want to put this student in: ";
+        getline(cin,classname);
+        addStudentToClass(classname,tmpSt,data);
+        int tmp;
+        cout << "Do you want to continue(1.Yes 0.No): ";
+        cin >> tmp;
+        switch(tmp){
+            case 0:
+                return;
             default:
                 break;
         }
     }
-    if (choice != 2){
-        Student *curSt = tmpClass ->pHeadStudent;
-        if (curSt == NULL){
-            cout << "This class is empty";
-            //return;
+}
+
+void removeAStudent(CoreData &data){
+    while(1){
+        long long tmpid;
+        cout << "Please input the id whose you want to delete: ";
+        cin >> tmpid;
+        removeStudent(tmpid,data);
+        int tmp;
+        cout << "Do you want to continue(1.Yes 0.No): ";
+        cin >> tmp;
+        switch(tmp){
+            case 0:
+                return;
+            default:
+                break;
         }
-        while (curSt != NULL){
-            cout << curSt->id << " " << curSt ->lastName << " "<<curSt ->firstName<< " "<<curSt->dOB<<" "<<curSt->gender<<endl;
-            curSt = curSt ->next;
+    }
+}
+
+void moveStudentFromAToB (CoreData &data){
+    while(1){
+        long long id;
+        cout << "Please input the student's ID: ";
+        cin >> id;
+        Student *tmpSt;
+        Class *tmpClass;
+        string classmove;
+        if (findStudent(id,data,tmpSt,tmpClass)){
+            cout << "Name: " << tmpSt->lastName << " " << tmpSt->firstName << endl;
+            cout << "Class: "<< tmpClass ->name <<endl;
+            cout << "DoB: " << tmpSt->dOB<<endl;
+            cout <<"Gender: "<<tmpSt->gender<<endl;
+            cout << "Which class do you want this student to move to?" <<endl;
+            cin.ignore();
+            getline(cin,classmove);
+            addStudentToClass(classmove,tmpSt,data);
+            removeStudent(id,data);
+            int tmp;
+            cout << "Do you want to continue(1.Yes 0.No): ";
+            cin >> tmp;
+            switch(tmp){
+                case 0:
+                    return;
+                default:
+                    break;
+            }
+        }
+        else{
+            cout << "can not find student." <<endl;
+            cout <<"1.Input another id." <<endl;
+            cout <<"2.Return."<<endl;
+            int choice;
+            cin >> choice;
+            switch(choice){
+                case 2:
+                    return;
+                default:
+                    break;
+            }
         }
     }
 }
--- function_qhuy.h
+++ function_qhuy.h
@@ -6,9 +6,15 @@ void staffClassMenu (CoreData &data);
 
 void importStudentFromCsvFile (CoreData &data);
 
-void edit(Student *&tmpSt,int choice);
-void editAnExistingStudent ( CoreData &data);
+void addAStudent(CoreData &data);
+
+void removeAStudent(CoreData &data);
+
+void moveStudentFromAToB (CoreData &data);
+
+void edit(Student *&tmpSt,int choice,bool &checkChoice);
+void editAnExistingStudent (CoreData &data);
 
 void viewListOfClasses(CoreData data);
-void viewListOfStudents(CoreData data);
+void viewListOfStudents(Class *curClass);
 #endif // FUNCTION_QHUY_H_INCLUDED
